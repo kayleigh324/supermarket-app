6e7fea60d7598c0e0a315933f671bb716814f921
Kayleigh Tuppen <kayleightuppen@gmail.com>
2023-11-21 21:18:08 +0000

    creating component for header and css

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div>Logo</div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Products</li>
+          <li>Basket</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
