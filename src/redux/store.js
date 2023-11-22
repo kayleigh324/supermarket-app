a4553e99e742d07d84864176edb681bc31509a3b
Kayleigh Tuppen <kayleightuppen@gmail.com>
2023-11-22 15:32:05 +0000

    updating store.js with devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,26 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import { persistStore, persistReducer } from 'redux-persist';
 import persistConfig from './persistConfig';
 
+const loggerMiddleware = (store) => (next) => (action) => {
+  console.log('Dispatching action:', action);
+  const result = next(action);
+  console.log('State after action:', store.getState());
+  return result;
+};
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const store = createStore(persistedReducer);
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  persistedReducer,
+  composeEnhancers(applyMiddleware(loggerMiddleware, thunk))
+);
+
 const persistor = persistStore(store);
 
 export { store, persistor };
