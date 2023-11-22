3251757404d31caba2c9ce012bfa933b40e53626
Kayleigh Tuppen <kayleightuppen@gmail.com>
2023-11-22 15:31:31 +0000

    removing extra props

--- src/Components/Basket/Basket.js
+++ src/Components/Basket/Basket.js
@@ -1,9 +1,14 @@
-// Basket.js
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-const Basket = ({ onRemoveFromBasket }) => {
+const Basket = () => {
+  const dispatch = useDispatch();
   const items = useSelector((state) => state.basket.items) || [];
+
+  const handleClick = (item) => {
+    console.log('Remove clicked:', item);
+    dispatch({ type: 'REMOVE_FROM_BASKET', payload: item });
+  };
 
   return (
     <div>
@@ -12,7 +17,7 @@ const Basket = ({ onRemoveFromBasket }) => {
         {items.map((item) => (
           <li key={item.id}>
             {item.name} - ${item.price} (Quantity: {item.quantity})
-          <button onClick={() => onRemoveFromBasket(item)}>Remove</button>
+            <button onClick={() => handleClick(item)}>Remove</button>
           </li>
         ))}
       </ul>
