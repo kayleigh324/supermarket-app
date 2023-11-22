66e5ac038dde910f83928b74685e22f6188c1897
Kayleigh Tuppen <kayleightuppen@gmail.com>
2023-11-22 08:38:21 +0000

    deleting unused code

--- src/Components/ProductList/ProductList.js
+++ src/Components/ProductList/ProductList.js
@@ -55,49 +55,4 @@ const ProductList = () => {
 
 export default ProductList;
 
-/*import React, { useState, useEffect } from 'react';
-
-const ProductList = () => {
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://s3.eu-west-2.amazonaws.com/techassessment.cognitoedu.org/products.json');
-        const data = await response.json();
-        setProducts(data);
-        console.log(data);
-      } catch (error) {
-        setError(error.message);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-  
-
-  return (
-    <div>
-      <h1>Product List</h1>
-      {Array.isArray(products) && products.length > 0 ? (
-        <ul>
-          {products.map((product) => (
-            <li key={product.id}>{product.name}</li>
-          ))}
-        </ul>
-      ) : (
-        <div>No products available.</div>
-      )}
-    </div>
-  );
-};
-
-export default ProductList;
-*/
-
 
