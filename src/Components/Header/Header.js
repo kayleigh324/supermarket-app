f18b57134d88bb22ccfcf75176e32a2abcaff41a
Kayleigh Tuppen <kayleightuppen@gmail.com>
2023-11-22 15:46:07 +0000

    updating state in Header.js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import './Header.css';
 
 const Header = () => {
+  const totalItems = useSelector((state) => state.basket.totalItems) || 0;
+
   return (
     <header className="header">
       <div className="logo">Logo</div>
@@ -10,7 +13,7 @@ const Header = () => {
         <ul className="nav-list">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/products">Products</Link></li>
-          <li><Link to="/basket">Basket</Link></li>
+          <li><Link to="/basket">Basket ({totalItems} items)</Link></li>
         </ul>
       </nav>
     </header>
