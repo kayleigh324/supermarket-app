39c73da1c492789c05ff5a83c06c5fa0813a70c2
Kayleigh Tuppen <kayleightuppen@gmail.com>
2023-11-22 15:47:38 +0000

    updating basketReducer to reflect UI

--- src/redux/reducers/basketReducer.js
+++ src/redux/reducers/basketReducer.js
@@ -1,15 +1,18 @@
 const initialState = {
   items: [],
+  totalItems: 0,
 };
 
 const basketReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_TO_BASKET':
       const existingItem = state.items.find((item) => item.id === action.payload.id);
-    //check if item already exists in basket, add quantity 
+    //check if item already exists in basket, add quantity
       if (existingItem) {
         return {
           ...state,
+          //updating UI basket
+          totalItems: state.totalItems + 1,
           items: state.items.map((item) =>
             item.id === action.payload.id ? { ...item, quantity: item.quantity + 1 } : item
           ),
@@ -17,10 +20,12 @@ const basketReducer = (state = initialState, action) => {
       } else {
         return {
           ...state,
+          //updating UI basket with -1
+          totalItems: state.totalItems + 1,
           items: [...state.items, { ...action.payload, quantity: 1 }],
         };
       }
-      //reducer to remove from basket, reducing quantity, then item once it is 1
+//reducer to remove from basket, reducing quantity, then item once it is 1
     case 'REMOVE_FROM_BASKET':
       const updatedItems = state.items.reduce((acc, item) => {
         if (item.id === action.payload.id && item.quantity > 1) {
@@ -33,10 +38,10 @@ const basketReducer = (state = initialState, action) => {
 
       return {
         ...state,
+        totalItems: Math.max(0, state.totalItems - 1),
         items: updatedItems,
       };
 
-    // Add more cases for other actions if needed
     default:
       return state;
   }
