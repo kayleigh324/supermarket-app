7feec1253c3eb3cfdfa1e90ec9adf0bdf29901d6
Kayleigh Tuppen <kayleightuppen@gmail.com>
2023-11-22 17:28:29 +0000

    adding styling and logic

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
-const Header = () => {
+
+  const Header = () => {
   const totalItems = useSelector((state) => state.basket.totalItems) || 0;
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
-    <header className="header">
-      <div className="logo">Logo</div>
-      <nav>
-        <ul className="nav-list">
+    <header className={`header ${menuOpen ? 'menu-open' : ''}`}>
+      <div className="logo">
+        <img
+          className="logo-image"
+          src="/images/cognito_logo_brick_small_1.png"
+          alt="Logo"
+        />
+      </div>
+      <div className="menu-icon" onClick={handleMenuClick}>
+        <div className={`bar ${menuOpen ? 'animate' : ''}`}></div>
+        <div className={`bar ${menuOpen ? 'animate' : ''}`}></div>
+        <div className={`bar ${menuOpen ? 'animate' : ''}`}></div>
+      </div>
+      <nav className={`nav-list ${menuOpen ? 'show' : ''}`}>
+        <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/products">Products</Link></li>
           <li><Link to="/basket">Basket ({totalItems} items)</Link></li>
