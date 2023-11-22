245007c4b204c4fda087256f3b19c05c75e0a2a0
Kayleigh Tuppen <kayleightuppen@gmail.com>
2023-11-22 09:36:45 +0000

    removing basket comp from App.js to ProductList

--- src/App.js
+++ src/App.js
@@ -9,10 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <ProductDetail />
-      <ProductList />
-      
-      
-      
+      <ProductList />         
     </div>
   );
 }
