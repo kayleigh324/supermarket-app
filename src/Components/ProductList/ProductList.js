f380bdca96c078425e7aa64d8f61b848094a1b40
Kayleigh Tuppen <kayleightuppen@gmail.com>
2023-11-22 08:38:07 +0000

    updating with correct logic

--- src/Components/ProductList/ProductList.js
+++ src/Components/ProductList/ProductList.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import ProductDetail from '../ProductDetail/ProductDetail';
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(null);
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
+ 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch('https://s3.eu-west-2.amazonaws.com/techassessment.cognitoedu.org/products.json');
         const data = await response.json();
         console.log(data)
-        setProducts(data.products);
+        setProducts(data);
       } catch (error) {
         setError(error.message);
       }
@@ -24,15 +27,67 @@ const ProductList = () => {
     return <div>Error: {error}</div>;
   }
 
+  const handleProductClick = (product) => {
+    setSelectedProduct(product);
+  };
+
+
+  return (
+    <div>
+      <h1>Product List</h1>
+      {Array.isArray(products) && products.length > 0 ? (
+        <>
+          <ul>
+            {products.map((product) => (
+              <li key={product.id} onClick={() => handleProductClick(product)}>
+                {product.name} - ${product.price}
+              </li>
+            ))}
+          </ul>
+          {selectedProduct && <ProductDetail product={selectedProduct} />}
+        </>
+      ) : (
+        <div>No products available.</div>
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
+
+/*import React, { useState, useEffect } from 'react';
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://s3.eu-west-2.amazonaws.com/techassessment.cognitoedu.org/products.json');
+        const data = await response.json();
+        setProducts(data);
+        console.log(data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+  
+
   return (
     <div>
       <h1>Product List</h1>
       {Array.isArray(products) && products.length > 0 ? (
         <ul>
           {products.map((product) => (
-            <li key={product.id}>
-              {product.name} - ${product.price}
-            </li>
+            <li key={product.id}>{product.name}</li>
           ))}
         </ul>
       ) : (
@@ -43,4 +98,6 @@ const ProductList = () => {
 };
 
 export default ProductList;
+*/
+
 
