cfc626b75062a617c42b666c27d41536e1d507d8
Kayleigh Tuppen <kayleightuppen@gmail.com>
2023-11-22 11:38:31 +0000

    adding navbar routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import ProductDetail from './Components/ProductDetail/ProductDetail';
 import Basket from './Components/Basket/Basket';
 import Header from './Components/Header/Header';
 import ProductList from './Components/ProductList/ProductList';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <ProductDetail />
-      <ProductList />         
-    </div>
+      <Routes>
+        <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/basket" element={<Basket />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
