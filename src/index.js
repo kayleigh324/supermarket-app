66dcafbc739cb78a041109bc381e94dcdb625cea
Kayleigh Tuppen <kayleightuppen@gmail.com>
2023-11-22 09:56:15 +0000

    updates to index.js for redux-persist

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { StrictMode } from 'react';
 import App from './App';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 import reportWebVitals from './reportWebVitals';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <App />
+      </PersistGate>
     </Provider>
   </StrictMode>,
 );
