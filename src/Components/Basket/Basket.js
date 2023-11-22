ebea52dd85245ade6b9673ae26f3a036822140d1
Kayleigh Tuppen <kayleightuppen@gmail.com>
2023-11-22 09:35:06 +0000

    updating to remove from basket

--- src/Components/Basket/Basket.js
+++ src/Components/Basket/Basket.js
@@ -1,12 +1,18 @@
+// Basket.js
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Basket = ({ items }) => {
+const Basket = ({ onRemoveFromBasket }) => {
+  const items = useSelector((state) => state.basket.items);
+
   return (
     <div>
       <h2>Basket</h2>
       <ul>
         {items.map((item) => (
-          <li key={item.id}>{item.name} - ${item.price}</li>
+          <li key={item.id}>{item.name} - ${item.price}
+          <button onClick={() => onRemoveFromBasket(item)}>Remove</button>
+          </li>
         ))}
       </ul>
     </div>
