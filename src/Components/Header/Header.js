af91e1736936b45b265191e51e10fdef7b9c2e90
Kayleigh Tuppen <kayleightuppen@gmail.com>
2023-11-24 09:38:42 +0000

    removing unused code

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,39 +1,3 @@
-/*import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import './Header.css';
-import logoImage from '../../assets/images/cognito_logo_brick_small_1.png'
-
-
-  const Header = () => {
-  const totalItems = useSelector((state) => state.basket.totalItems) || 0;
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const handleMenuClick = () => {
-    setMenuOpen(!menuOpen);
-  };
-
-  return (
-    <header className={`header ${menuOpen ? 'menu-open' : ''}`}>
-      <div className="logo">
-      <img className="jss507" src={logoImage} alt="Logo" />
-      </div>
-      <div className="menu-icon" onClick={handleMenuClick}>
-        <div className={`bar ${menuOpen ? 'animate' : ''}`}></div>
-        <div className={`bar ${menuOpen ? 'animate' : ''}`}></div>
-        <div className={`bar ${menuOpen ? 'animate' : ''}`}></div>
-      </div>
-      <nav className={`nav-list ${menuOpen ? 'show' : ''}`}>
-        <ul>
-          <li><Link to="/">Browse Products</Link></li>
-          <li><Link to="/basket">Basket ({totalItems} items)</Link></li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
-
-export default Header;*/
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
