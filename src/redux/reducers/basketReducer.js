1e9ef0fc6ab8b0243be5767e475da509bf4c6b51
Kayleigh Tuppen <kayleightuppen@gmail.com>
2023-12-12 16:22:18 +0000

    adding comments

--- src/redux/reducers/basketReducer.js
+++ src/redux/reducers/basketReducer.js
@@ -3,6 +3,7 @@ const initialState = {
   totalItems: 0,
 };
 
+//reducer for "basket". Takes current state and action as params. state is initialized with initial state if empty
 const basketReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_TO_BASKET':
@@ -20,7 +21,7 @@ const basketReducer = (state = initialState, action) => {
       } else {
         return {
           ...state,
-          //updating UI basket with -1
+          //update state for new item
           totalItems: state.totalItems + 1,
           items: [...state.items, { ...action.payload, quantity: 1 }],
         };
@@ -28,6 +29,8 @@ const basketReducer = (state = initialState, action) => {
 //reducer to remove from basket, reducing quantity, then item once it is 1
     case 'REMOVE_FROM_BASKET':
       const updatedItems = state.items.reduce((acc, item) => {
+        //checks if current item in basket has same ID as in action payload id. 
+        //Also checks if > 1. If so, reduce qty by 1 and pushes updated item to accumulator
         if (item.id === action.payload.id && item.quantity > 1) {
           acc.push({ ...item, quantity: item.quantity - 1 });
         } else if (item.id !== action.payload.id) {
